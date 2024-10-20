dae6e28f00a8ba8cd79c652b1d8bda30a8e9c64b
Dodo <edozompanti@gmail.com>
2024-10-20 18:13:06 +0200

    implementate alcune funzioni per il buddy

--- buddy_allocator.c
+++ buddy_allocator.c
@@ -4,4 +4,44 @@
 #include <string.h>
 #include "buddy_allocator.h"
 
+int get_level(int index) {
+    int level = 0;
+    while ((1 << (level + 1)) - 1 <= index) {
+        level++;
+    }
+    return level;
+}
+
+int get_parent(int index) {
+    if (index == 0) {
+        return -1; 
+    }
+    return (index - 1) / 2;
+}
+
+int get_buddy(int index) {
+    if (index % 2 == 0) {
+        return index - 1; 
+    } else {
+        return index + 1;  
+    }
+}
+
+void buddy_allocator_init(BuddyAllocator *buddy_allocator, bitmap *bitmap, char *mem, int num_levels, int min) {
+    buddy_allocator->mem = mem;        
+    buddy_allocator->num_levels = num_levels;
+    buddy_allocator->min_bucket_size = min;
+
+    int num_blocks = (1 << buddy_allocator->num_levels) - 1;
+
+    uint8_t* buffer = (uint8_t*)malloc(get_bytes(num_blocks));  
+    bitmap_init(bitmap, buffer, num_blocks);  
+
+    buddy_allocator->bit_map = *bitmap;   
+
+    for (int i = 0; i < num_blocks; i++) {
+        set_bit(&buddy_allocator->bit_map, i, STATUS_OFF);  
+    }
+}
+
 
--- buddy_allocator.h
+++ buddy_allocator.h
@@ -7,7 +7,7 @@ typedef struct {
   int min_bucket_size;
 } BuddyAllocator;
 
-void BuddyAllocator_init(BuddyAllocator* buddyallocator, int total_memory_size, int min_bucket_size);
+void buddy_allocator_init(BuddyAllocator *buddy_allocator, bitmap *bitmap, char *memory, int lvl, int min); 
 
 int BuddyAllocator_alloc(BuddyAllocator* buddyallocator, int size);
 
@@ -16,5 +16,11 @@ void BuddyAllocator_free(BuddyAllocator* buddyallocator, int index);
 int BuddyAllocator_get_block_size(BuddyAllocator* buddyallocator, int level);
 
 int BuddyAllocator_is_block_free(BuddyAllocator* buddyallocator, int index);
+
+int get_level(int index);
+
+int get_parent(int index);
+
+int get_buddy(int index);
 
 
